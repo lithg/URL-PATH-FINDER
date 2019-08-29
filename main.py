591ad47e4a3574a89240830b47741a999c01b534
Guilherme Carvalho <gui.cpc@live.com>
2019-08-29 12:27:45 -0300

    Added terminal args

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ from urllib.error import URLError, HTTPError
 import time
 from datetime import datetime, timedelta
 from colorama import Fore
+import argparse
+import sys
+
+
+# -----------------------------------------------------
 
 print(Fore.RED + '''
  ██▓███   ▄▄▄     ▄▄▄█████▓ ██░ ██      █████▒██▓ ███▄    █ ▓█████▄ ▓█████  ██▀███  
@@ -23,11 +28,15 @@ print(Fore.RED + '''
 ░░         ░   ▒    ░       ░  ░░ ░    ░ ░    ▒ ░   ░   ░ ░  ░ ░  ░    ░     ░░   ░        by ''' + Fore.GREEN + 'LITHG' + '''
                ░  ░         ░  ░  ░           ░           ░    ░       ░  ░   ░     
                                                              ░                     
-''')
+\n''')
+
+print(Fore.YELLOW + "        ################# - Interesting Paths Finder - #################")
+print(Fore.YELLOW + "        #                Works better with python 3.x                  #")
+print(Fore.YELLOW + "        #           Usage: python3 main.py -u <url> -l <wordlist>      #")
+print(Fore.YELLOW + "        ################################################################\n\n")
 
 
-url = ''
-wordlist = 'paths.txt'
+# -----------------------------------------------------
 
 
 def format_url(url):
@@ -36,6 +45,8 @@ def format_url(url):
         url = url + '/'
 
     return url
+
+# -----------------------------------------------------
 
 
 def read_wordlist():
@@ -46,7 +57,9 @@ def read_wordlist():
             lines = path.readlines()
 
     except:
-        print('Error while trying to read file.')
+        print(Fore.RED + 'Error while trying to read wordlist.')
+
+# -----------------------------------------------------
 
 
 def timing(total_time):
@@ -54,9 +67,11 @@ def timing(total_time):
     sec = timedelta(seconds=total_time)
     d = datetime(1,1,1) + sec
 
-    total_time = "\nTiming: %dd:%dh:%dm:%ds" % (d.day-1, d.hour, d.minute, d.second)
+    total_time = Fore.BLUE + "\nTiming: %dd:%dh:%dm:%ds" % (d.day-1, d.hour, d.minute, d.second)
 
     return total_time
+
+# -----------------------------------------------------
 
 
 def find_paths():
@@ -70,35 +85,58 @@ def find_paths():
 
     for path in lines:
         path = path.rstrip()
+        count += 1
 
         try:
-            urllib.request.urlopen(format_url(url) + path).getcode()
-            print('Panel found! Path:', path)
+
+            urllib.request.urlopen(format_url(url) + path)
+            print(Fore.YELLOW + '[' + Fore.GREEN + str(count) + Fore.YELLOW + ']', Fore.GREEN + 'Panel found! Path:', path)
             found.append(path)
 
         except HTTPError:
-            count += 1
-            print('[' + str(count) + ']', 'Failed:', format_url(url) + path)
+            print(Fore.YELLOW + '[' + Fore.BLUE + str(count) + Fore.YELLOW + ']', Fore.RED + 'Failed:', format_url(url) + path)
 
         except URLError as e:
             print('Cannot run the script properly. Probably a URL issue.')
-            print('Reason:', e)
+            print(Fore.RED + 'Reason:', e)
             break
 
     end = time.time()
     total_time = end - start
 
     if len(found) > 0:
-        print('#-#-#-#-# [' + str(len(found)) + '] PATH FOUND(s) #-#-#-#-#')
+        print(Fore.GREEN + '#-#-#-#-# [' + Fore.BLUE +str(len(found)) + Fore.GREEN + '] PATH FOUND(s) #-#-#-#-#')
         for panel in found:
             found_count += 1
             print('[' + str(found_count) + ']', url + path)
 
     else:
-        print('Cannot find any path. Try another wordlist!')
+        print(Fore.RED + '\nCannot find any path. Try another wordlist!')
 
     print(timing(total_time))
 
 
+# -----------------------------------------------------
+
+
 if __name__ == '__main__':
-    find_paths()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '-url', nargs=1, help='Base URL', default=None, type=str)
+    parser.add_argument('-l', '-list', nargs=1, help='Wordlist to use', default="paths.txt", type=str)
+    args = parser.parse_args()
+
+    if not args.u:
+        print(Fore.RED + '***********************************')
+        print(Fore.RED + '* URL required to run the script! *')
+        print(Fore.RED + '***********************************')
+        print(Fore.YELLOW + 'Usage: python3 main.py -u <url> -l <wordlist> \n')
+
+    else:
+        url = args.u[0]
+
+    if args.l:
+        wordlist = args.l
+
+    if len(sys.argv) > 1:
+        find_paths()
+
